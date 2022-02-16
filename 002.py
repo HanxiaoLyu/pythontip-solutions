3e31bd5617dd94eb884c30efa84edb4cde29a9e6
Hanxiao Lyu <42114189+HanxiaoLyu@users.noreply.github.com>
2022-02-16 15:02:57 -0800

    Create 002.py

---
+++ 002.py
@@ -0,0 +1,11 @@
+#参考代码，如嫌啰嗦，建议删除，一行搞定
+def solve_it():
+    '''
+    pythontip oj不同于传统oj，代码里面直接使用变量，无需要提前声明，免去复杂的输入解析
+    life is short, so i user python~
+    you can use variables L
+    '''
+    
+    return sorted(L)
+
+print(solve_it())  # 答案需要输出
