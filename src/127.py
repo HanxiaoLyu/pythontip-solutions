1a47ddd60b4fc4b5527a2a753bf43b6ba2a6ceaa
Hanxiao Lyu <363204394@qq.com>
2022-02-22 16:04:16 -0800

    050

---
+++ src/127.py
@@ -0,0 +1,15 @@
+def get_one_from(a):
+    ret = 0
+    while a > 0:
+        if a % 10 == 1:
+            ret += 1
+        a = int(a / 10)
+    return ret
+
+def solve_it():
+    sum = 0
+    for i in range(1, n + 1):
+        sum += get_one_from(i)
+    return sum
+
+print(solve_it())
