edfdc78f8606745026aab04bfaf47139d61ab1d1
Hanxiao Lyu <363204394@qq.com>
2022-03-26 19:39:13 -0700

    050

--- src/21ex.py
+++ src/21ex.py
@@ -1,18 +1,21 @@
 a = "21abcba"
 n = 5
+
 def check(x):
     for i in range(n):
-        print(x+i, x+n-1-i)
-
-        if a[x + i] != a[x + n -1 - i]:
+        if a[x + i] != a[x + n - 1 - i]:
             return False
     return True
 
+
+
+
 def solve():
-    for i in range(len(a)- n + 1):
+    for i in range(len(a) - n + 1):
         if check(i):
             return True
     return False
+
 
 if solve():
     print("YES")
